a9a3d95ca345384a4bd7a1f5c34e385318091f2a
Smit <smitmpatil123@gmail.com>
2024-03-28 17:57:53 +0530

    App Setup

--- src/app.js
+++ src/app.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const cors = require("cors");
+const cookieParser = require("cookie-parser");
 
 const app = express();
 
@@ -11,6 +12,7 @@ app.use(cors({
 app.use(express.json());
 app.use(express.urlencoded({ extended: true, limit: "16kb"}));
 app.use(express.static("public"));
+app.use(cookieParser());
 
 const PORT = process.env.PORT || 3000;
 
